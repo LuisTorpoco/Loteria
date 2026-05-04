69584dd6f8fa8c59bdbf8dd77819c000ba26bbf1
SOFTTEKPROJECTS\juand.castillo <juand.castillo@softtek.com>
2026-05-04 12:24:47 +0200

    terminado la clase user

--- src/main/java/com/helloword/loteria/entity/User.java
+++ src/main/java/com/helloword/loteria/entity/User.java
@@ -1,4 +1,28 @@
 package com.helloword.loteria.entity;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+
+    @Id
+    private String id;
+    private String name;
+
+    @ElementCollection
+    private List<List<Integer>> apuestas=new ArrayList<>();
+
+    protected User(String id,String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public User(){
+
+    }
+
 }
