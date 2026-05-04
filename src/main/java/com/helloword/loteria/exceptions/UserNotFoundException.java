089f476d51fd4f55b376f1413361fb73bcd8fe4b
SOFTTEKPROJECTS\luis.torpoco <luis.torpoco@softtek.com>
2026-05-04 12:58:08 +0200

    exceptiones de notfound y alreadyexists

---
+++ src/main/java/com/helloword/loteria/exceptions/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.helloword.loteria.exceptions;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+}
